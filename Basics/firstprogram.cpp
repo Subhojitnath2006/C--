3574a89542df0e157f5fa88f743d0c23279d9043
Subhojit <rabinath750@gmail.com>
2024-04-18 07:07:55 +0530

    first program

---
+++ Basics/firstprogram.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // cout<<"My name is Subhojit";
+    // cout<<endl; //to enter a new line.
+    // cout<<"I am a good boy";
+
+    //another way
+    cout<<"Subhojit"<<endl<<"Nath";
+}
