6d4378898193bd853ea7ff68b9038eda8975f94a
Subhojit <rabinath750@gmail.com>
2024-04-20 07:37:32 +0530

    basics

---
+++ updatevariables.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x=5;
+    cout<<x<<endl;
+    x=x+7;
+    cout<<x<<endl;
+}
--- variables.cpp
+++ variables.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
+    // int x,y; //declaration
+    // x=5; //initialization
+    // y=9;
+
+    int x=5,y=9;
+    cout<<x+y;
 }
