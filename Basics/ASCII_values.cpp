2dc300e78d07e7b450ab93743de38f0522afa33c
Subhojit <rabinath750@gmail.com>
2024-04-21 09:01:56 +0530

    basics

---
+++ Basics/ASCII_values.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    // char ch;
+    // cout<<"enter a character: ";
+    // cin>>ch;
+    // cout<<"ASCII value is"<<(int)ch; //char to int.
+
+
+    int n;
+    cout<<"enter a number: ";
+    cin>>n;
+    cout<<"character is: "<<(char)n;
+    return 0;
+}
