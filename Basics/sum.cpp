d7c5ec842e3db3ea6ffb1f96e4c5b253339776e5
Subhojit <rabinath750@gmail.com>
2024-04-21 08:07:21 +0530

    taking input

---
+++ Basics/sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int x;
+    cout<<"enter first number: ";
+    cin>>x; // >> use to take input.
+    int y;
+    cout<<"enter second number: ";
+    cin>>y; // >> use to take input.
+
+    cout<<"the sum is: ";
+    cout<<x+y;
+    return 0;
+}
---
+++ Basics/taking_input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int x;
+    cout<<"enter a number: ";
+    cin>>x; // >> use to take input.
+    cout<<"the square is: ";
+    cout<<x*x; //<< use to print output.
+    return 0;
+}
