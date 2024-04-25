4dc2cb45ebc899567700d5d3430e8fa48fc2f9a0
Subhojit <rabinath750@gmail.com>
2024-04-25 06:59:09 +0530

    ternary operator

---
+++ Conditionals/ternary_operator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+   int n;
+   cout<<"enter a number: ";
+   cin>>n;
+   // syntax :
+   // (condition) ? if true : if false;
+   (n%2==0) ? cout<<"even" : cout<<"odd" ;
+    
+}
