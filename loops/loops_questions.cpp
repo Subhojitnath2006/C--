f2b30d48ebb1352f2ef63553e6a6b637eb3b9894
Subhojit <rabinath750@gmail.com>
2024-04-25 07:51:22 +0530

    for loop

---
+++ loops/loops_questions.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"enter a number: ";
+cin>>n;
+    for(int i=0;i<n;i++){
+        cout<< (2*i)+1 <<endl;
+    }
+    return 0;
+}
