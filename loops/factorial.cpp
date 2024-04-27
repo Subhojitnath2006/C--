8bb95049a679f773760f7bdb319b131dd9f0f279
Subhojit <rabinath750@gmail.com>
2024-04-27 07:44:46 +0530

    factorial

--- loops/factorial.cpp
+++ loops/factorial.cpp
@@ -1,3 +1,19 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+
+//     int n;
+//     cout<<"enter a number: ";
+//     cin>>n;
+   
+//    int fac= 1;
+//    for(int i=1;i<=n;i++){
+//     fac= fac*i;
+//    }
+//     cout<<"Factorial is: "<<fac;
+//     return  0;
+// }
+
 #include <iostream>
 using namespace std;
 int main(){
@@ -9,7 +25,8 @@ int main(){
    int fac= 1;
    for(int i=1;i<=n;i++){
     fac= fac*i;
+    cout<<"Factorial of "<<i<<"is: "<<fac<<endl;
    }
-    cout<<"Factorial is: "<<fac;
+    
     return  0;
 }
