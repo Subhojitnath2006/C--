76e6c9e8e6602deb3b6bb38b6b80194eecec2aad
Subhojit <rabinath750@gmail.com>
2024-04-27 07:42:47 +0530

    factorial

---
+++ loops/factorial.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int n;
+    cout<<"enter a number: ";
+    cin>>n;
+   
+   int fac= 1;
+   for(int i=1;i<=n;i++){
+    fac= fac*i;
+   }
+    cout<<"Factorial is: "<<fac;
+    return  0;
+}
