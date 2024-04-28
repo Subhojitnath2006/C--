253f9d260b6b2921d601127c792db6dcece1b268
Subhojit <rabinath750@gmail.com>
2024-04-28 10:32:57 +0530

    functions

---
+++ function_and_pointes/library_functions.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    cout<<sqrt(4.6)<<endl;
+    cout<<cbrt(8)<<endl;
+    
+    cout<<min(4,8)<<endl;
+    cout<<max(8,3)<<endl;
+    cout<<pow(2,6)<<endl;
+}
