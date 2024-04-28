fbb41802df37c8fd4cbd7068a7d4684d30f821ea
Subhojit <rabinath750@gmail.com>
2024-04-28 11:10:11 +0530

    pascals triangle

---
+++ function_and_pointes/pascalTriangle.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int fac(int x){
+    int fac=1;
+    for(int i=1;i<=x;i++){
+        fac*=i;
+    }
+    return fac;
+}
+int main(){
+   int n;
+   cout<<"enter no of rows: ";
+   cin>>n;
+
+   for(int i=0;i<n;i++){
+
+    for(int j=0;j<=n-i;j++){
+        cout<<"  ";
+    }
+    for(int j=0;j<=i;j++){
+        
+        cout<< (fac(i))/(fac(j)*fac(i-j))<<"  ";
+    }
+    cout<<endl;
+   }
+   return 0;
+}
