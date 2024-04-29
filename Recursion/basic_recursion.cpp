ba2f897394faf42a02834e71176649fbba37ac92
Subhojit <rabinath750@gmail.com>
2024-04-29 06:31:15 +0530

    recursion

--- Recursion/basic_recursion.cpp
+++ Recursion/basic_recursion.cpp
@@ -1,18 +1,40 @@
+// #include <iostream>
+// using namespace std;
+
+// void printNto1(int n){
+//     if (n==0) return;
+//     cout<<n<<endl;
+//     printNto1(n-1);
+    
+// }
+// int main()
+// {
+//  int n;
+//  cout<<"enter a number: ";
+//  cin>>n;
+// printNto1(n); 
+
+//     return 0;
+// }
+
+// print 1 to n using recursion
+
 #include <iostream>
 using namespace std;
 
-void printNto1(int n){
-    if (n==0) return;
-    cout<<n<<endl;
-    printNto1(n-1);
+void print1toN(int x,int n){
+    if (x>n) return;
+    cout<<x<<endl;
+    print1toN(x+1,n);
     
 }
 int main()
 {
+    
  int n;
  cout<<"enter a number: ";
  cin>>n;
-printNto1(n); 
+ print1toN(1,n); 
 
     return 0;
 }
