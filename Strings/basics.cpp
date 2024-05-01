4aa4625d765e16138888ea0c630c356e5a450d95
Subhojit <rabinath750@gmail.com>
2024-05-01 06:29:15 +0530

    strings

---
+++ Strings/basics.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    // string x="Subhojit Nath";
+
+    string s;
+    cout<<"enter a string: ";
+    cin>>s;
+
+    cout<<s;
+    return 0;
+}
